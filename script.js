979eecaa59e33526d9e59c448dac27c12e9c51bd
unknown <https://github.com/AlessandrooGomess>
2025-11-08 14:48:24 -0300

    Validação de campos

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    if (form) {
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            const email = document.getElementById("email").value.trim();
-            const senha = document.getElementById("senha").value.trim();
+  const form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const email = document.getElementById("email").value.trim();
+      const senha = document.getElementById("senha").value.trim();
 
-            if (email === "alessandro@gmail.com" && senha === "123") {
-                window.location.href = "index.html";
-            } else {
-                alert("Usuário ou senha incorretos!");
-            }
-        });
-    }
+      if (email === "alessandro@gmail.com" && senha === "123") {
+        window.location.href = "index.html";
+      } else {
+        alert("Usuário ou senha incorretos!");
+      }
+    });
+  }
 
-    const botaoSair = document.getElementById("botao-sair");
-    if (botaoSair) {
-        botaoSair.addEventListener("click", () => {
-            window.location.href = "login.html";
-        });
-    }
+  const botaoSair = document.getElementById("botao-sair");
+  if (botaoSair) {
+    botaoSair.addEventListener("click", () => {
+      window.location.href = "login.html";
+    });
+  }
 });
 
-const inputs = document.querySelectorAll(".numero input");
-const mensagem = document.getElementById("mensagem");
+document.getElementById('preco').addEventListener('keypress', function (event) {
 
-inputs.forEach((input) => {
-  input.addEventListener("input", function (e) {
+  const permitidos = "0123456789.,";
 
-    const valorLimpo = e.target.value.replace(/[^0-9.,]/g, '');
+  if(!permitidos.includes(event.key)){
+    event.preventDefault();
+    alert('❌ Este campo só aceita números!');
+  }
+})
 
-    if (e.target.value !== valorLimpo) {
-      mensagem.style.display = "block";
-      e.target.value = valorLimpo;
-    } else {
-      mensagem.style.display = "none";
-    }
+document.getElementById('quantidade').addEventListener('keypress', function(event){
 
-    const partes = valorLimpo.split(/[.,]/);
-    if (partes.length > 2) {
-      e.target.value = partes[0] + "." + partes[1];
-    }
-  });
-});
+  const permitidos ='0123456789';
+
+  if(!permitidos.includes(event.key)){
+    event.preventDefault();
+    alert('❌ Este campo só aceita números!');
+  }
+})
